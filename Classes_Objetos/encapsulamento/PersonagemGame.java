b2dd69734b806c9c0076ce62549cd95124d44ae3
Henrique Moreira Amorim <hen.moreira.a@gmail.com>
2025-07-11 18:09:53 +0000

    add code

--- Classes_Objetos/encapsulamento/PersonagemGame.java
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -17,22 +17,4 @@ public class PersonagemGame {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public void tomarDano(int quantidadeDeDano) {
-        int saudeFinal = saudeAtual - quantidadeDeDano;
-        if(saudeFinal >= 0) {
-            saudeAtual = saudeFinal;
-        } else {
-            saudeFinal = 0;
-        }
-    }
-
-    public void receberCura(int quantidadeDeCura) {
-        int saudeFinal = saudeAtual + quantidadeDeCura;
-        if(saudeFinal < 100) {
-            saudeAtual = saudeFinal;
-        } else {
-            saudeFinal = 100;
-        }
-    }
 }
