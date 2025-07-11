5ec9d90734801f728e103ba9cb1869cc16555920
Henrique Moreira Amorim <hen.moreira.a@gmail.com>
2025-07-11 19:24:20 +0000

    add code

---
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,18 @@
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double calcularTotal() {
+        double valorFinal = 0;
+        for(ItemPedido item: itens) {
+            double valorPorItem = item.getQuantidade() * item.getProduto().obterPrecoLiquido();
+            valorFinal += valorPorItem;
+        }
+        return valorFinal + (valorFinal * percentualDesconto);
+    }
+}
