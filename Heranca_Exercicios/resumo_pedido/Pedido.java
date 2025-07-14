566d8dd88469c08e6fa779dc0c2f16f72afb493b
Henrique Moreira Amorim <hen.moreira.a@gmail.com>
2025-07-14 23:33:54 +0000

    ajusta nomeclatura print tipo produto

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -33,7 +33,7 @@ public class Pedido {
     public void apresentarResumoPedido() {
         System.out.println("------- RESUMO PEDIDO -------");
         for(ItemPedido item: itens) {
-            System.out.println(String.format("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f", item.getProduto().getClass().getName(), item.getProduto().getTitulo(), item.getProduto().getPrecoBruto(), item.getQuantidade(), calcularValorPorItem(item)));
+            System.out.println(String.format("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f", item.getProduto().getClass().getSimpleName(), item.getProduto().getTitulo(), item.getProduto().getPrecoBruto(), item.getQuantidade(), calcularValorPorItem(item)));
         }
         System.out.println("----------------------------");
         System.out.println(String.format("DESCONTO: %.2f", calcularDesconto(getValorFinal())));
